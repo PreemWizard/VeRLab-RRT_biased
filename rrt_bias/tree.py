a2e3ca4d4f4690d253526e179998aef1cfbe4042
GFX-3000 <148722532+GFX-3000@users.noreply.github.com>
2026-04-06 09:03:36 -0300

    Grafo global e expansão de uma árvore de cada vez

--- rrt_bias/tree.py
+++ rrt_bias/tree.py
@@ -1,12 +1,19 @@
 import numpy as np
+from bias_roulett import Roulette
 
 class Tree:
-    def __init__(self, root : np.ndarray):
+    def __init__(self, root : np.ndarray, roulette : Roulette):
+        self.root = root
         self.vertexes = [root]
         self.edges = []
+        self.roulette = roulette
+        self.tree_connected_to = []
 
     def add_vertex(self, vertex : np.ndarray):
         self.vertexes.append(vertex)
 
     def add_edge(self, v_near : np.ndarray, v_new : np.ndarray):
         self.edges.append(np.array([v_near, v_new]))
+
+    def add_connected_tre(self, tree_root : list):
+        self.tree_connected_to.append(tree_root)
